2a6c42df9fb6367e3199908fb94b95d27d5c6094
droidbrown <android@hashbrown.in>
2017-06-21 12:07:23 +0530

    alignment

--- app/src/main/java/com/hashbrown/erebor/locationwisenew/views/activities/Activity_SavedImages.java
+++ app/src/main/java/com/hashbrown/erebor/locationwisenew/views/activities/Activity_SavedImages.java
@@ -268,7 +268,7 @@ public class Activity_SavedImages extends AppCompatActivity implements OnSavedCl
                         return false;
                     }
                 })
-                .crossFade(1000).dontAnimate()
+                .crossFade(500).dontAnimate()
 
                 .into(expandedImageView);
 
